8703d8f1c545cf235feddf1aba577dc8620b2e54
Chhaya Singh <166850490+Chhayasingh18@users.noreply.github.com>
2026-07-12 12:35:50 +0000

    variables and data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+//console.log(3+3) console.log("Chhaya") no code readability here, it should be high
+
+console.log(3 + 3) 
+console.log("Chhaya")
+
+let name = "chhaya"
+let age = 18
+let isLoggeIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value, its representation of empty value
+// undefined => variable declared but its value not assigned to it
+// symbol => unique
+
+
+// object
+
+console.log(typeof "chhaya") //string
+console.log(typeof age) //number 
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
